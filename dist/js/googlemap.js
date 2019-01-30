50d3d7cc6c125bae8b98b39a2a61bcec6d7701e3
KalderLeon <Levchenko586@gmail.com>
2019-01-30 19:02:27 +0200

    Crative site HTML/CSS+google API

---
+++ dist/js/googlemap.js
@@ -0,0 +1,11 @@
+function initMap() {
+        var uluru = {lat: 48.023, lng: 37.80224};
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 10,
+          center: uluru
+        });
+        var marker = new google.maps.Marker({
+          position: uluru,
+          map: map
+        });
+      }
